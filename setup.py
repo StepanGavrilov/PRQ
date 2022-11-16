b8f2d8acb19d788fc7f0c59e750a5b60a02dc01d
Stepan <55409307+StepanGavrilov@users.noreply.github.com>
2022-11-16 12:33:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="prq",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="0.0.19",
+    version="0.0.20",
     py_modules=["prq"],
     entry_points={
         "console_scripts": [
